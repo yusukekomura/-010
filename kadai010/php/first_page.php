09414d237efc065d428acfb55af08926c2040a76
yusukekomura <koumura0620@gmail.com>
2019-06-27 22:45:16 +0900

    提出

---
+++ kadai010/php/first_page.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>Test Page</title>
+        <meta name="description" content="">
+        <link rel="icon" type="image/png" href="images/favicon.png">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSS -->
+        <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Philosopher" rel="stylesheet">
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+    <body>
+        <div id="home" class="big-bg">
+            <header class="page-header wrapper">
+           
+                <nav>
+                    <ul class="main-nav">
+                     
+                        <li><a href="login.php" >ログインページへ</a></li>
+                        <li><a href="user.php">ユーザー登録</a></li>
+                    </ul>
+                </nav>
+            </header>
+
+            <div class="home-content wrapper">
+                <h2 class="page-title">There is not Never ending site</h2>
+                <p>施工現場の効率をUPする</p>
+                <a class="button" href="menu.html">メインページへ</a>
+            </div><!-- /.home-content -->
+        </div><!-- /#home -->
+    </body>
+</html>
